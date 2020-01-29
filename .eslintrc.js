967a3051a839884a775e971a6f3915661c6e1ef3
Thornton <thorfernbacher@gmail.com>
2020-01-29 14:09:35 -0500

    files

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/jsx-filename-extension": [
+			1,
+			{
+				"extensions": [
+					".js",
+					".jsx"
+				]
+			}
+    ],
+    "no-console": "warn",
+    "no-mixed-spaces-and-tabs": [
+			"error",
+			"smart-tabs"
+		]
+  },
+};
---
+++ src/pages/Login.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+
+const Login = () =>
+<h1>THIS IS THE LOGIN</h1>;
+
+export default Login;
